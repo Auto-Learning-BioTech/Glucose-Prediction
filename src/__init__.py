7e239e20aaf224ef6b7584e27c619b390107b130
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-03-29 19:00:51 -0600

    postman

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask      
+from flask import Flask, request
+import io
+import csv
 
 app = Flask(__name__)
 
@@ -10,5 +12,18 @@ def home():
 def salvador():
     return "Hello, Salvador"
     
+@app.route("/datasets", methods=["POST"])
+def post_datasets():
+    f = request.files['data_file']
+
+    stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+    csv_input = csv.reader(stream)
+    #print("file contents: ", file_contents)
+    #print(type(file_contents))
+    print(csv_input)
+    for row in csv_input:
+        print(row)
+    return ""
+
 if __name__ == "__main__":
     app.run(debug=True)
